d273e43ca4c05bdeb418272695795fd953c68c92
michaelpenta <mpenta@necc.mass.edu>
2019-12-23 18:49:23 -0500

    this killed me

--- AoC2019_day14.py
+++ AoC2019_day14.py
@@ -1,4 +1,5 @@
 import math
+from typing import *
 
 
 def wordnumpair(l):
@@ -6,82 +7,79 @@ def wordnumpair(l):
     k = k.split(" ")
     word = str(k[1])
     num = int(k[0])
-    return word, num
+    return (num, word)
 
 
 def load_data():
     data = {}
-    ores = {}
     f = open("input")
     lines = f.readlines()
     for line in lines:
         print(line)
         l = line[:-1]
         l = l.split("=>")
-        e = (key, count) = wordnumpair(l[1])
+        e = (count, key) = wordnumpair(l[1])
         if key not in data:
-            data[key] = (count, [])
+            data[key] = [count, []]
         else:
             data[key][1].append(e)
         c = l[0].split(", ")
         for u in c:
-            e = (word, count) = wordnumpair(u)
+            e = wordnumpair(u)
             data[key][1].append(e)
-
     return data
 
 
+def compute(amt_needed, chem):
+    global reactions
+    global excess
+    global results
 
+    reaction_data = reactions[chem]
+    n_pack = reaction_data[0]
+    reactant_list= reaction_data[1]
+    if reactant_list[0][1] == 'ORE':
+        results.append((amt_needed, chem))
+    elif chem in excess and excess[chem] >= amt_needed:
+        excess[chem] -= amt_needed
+    else:
+        if chem in excess:
+            amt_needed -= excess[chem]
+            excess[chem] = 0
 
+        whole_units = math.ceil(amt_needed/n_pack)
+        left = (whole_units * n_pack) - amt_needed
 
-
-def decompose(reactions, current, coef, result):
-    print("Current={0}\n\tCeof={1}".format(current, coef))
-    if type(current) == tuple and type(current[1]) == list:
-        terms = current[1]
-        for t in terms:
-            val = reactions[t[0]]
-            l = val[1]
-            if l[0][0] == "ORE":
-                result.append((t[0], coef * t[1]))
+        if left > 0:
+            if chem not in excess:
+                excess[chem] = left
             else:
-                unit = val[0]
-                qty = t[1] * coef
-                c = math.ceil(qty/unit)
-                decompose(reactions, reactions[t[0]],c, result)
-
-
-def find_like_terms(p):
-    result= {}
-    for term in p:
-        if term[0] in result:
-            result[term[0]] += term[1]
-        else:
-            result[term[0]] = term[1]
-    return result
-
-def calc_ores(reactions, dic):
-    ores = 0
-    for entry in dic:
-        val = reactions[entry]
-        m = math.ceil(dic[entry] / val[0])
-        ores += m * val[1][0][1]
-        print("val={0} coef={1}".format(val, m * val[1][0][1]))
-    return ores
-
-
-reacts = load_data()
-for r in reacts:
-    print(r + "<=" + str(reacts.get(r)))
+                excess[chem] +=  left
+        for reactant in reactant_list:
+            compute(reactant[0] * whole_units, reactant[1])
 
 
 
-primary = []
-decompose(reacts, reacts['FUEL'], 1, primary)
-print(primary)
-primary.sort(key=lambda x: x[0])
-print(primary)
-simple = find_like_terms(primary)
-print(simple)
-o = calc_ores(reacts, simple)
-print(o)
+
+reactions = load_data()
+excess = {}
+results = []
+for r in reactions:
+    print(r + "<=" + str(reactions.get(r)))
+
+compute(1, 'FUEL')
+print(excess)
+print(results)
+
+like_terms = {}
+for r in results:
+    if r[1] in like_terms:
+        like_terms[r[1]] += r[0]
+    else:
+        like_terms[r[1]] = r[0]
+
+print(like_terms)
+
+l = list(map(lambda k: reactions[k][1][0][0] * math.ceil(like_terms[k]/reactions[k][0]), like_terms))
+
+print(sum(l))
